3fa42d9bdfca995796b03e88db49ee9e45a28403
jpcutshall <jpcutshall@gmail.com>
2021-07-15 15:44:39 -0400

    style update

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -6,7 +6,7 @@ interface Props {
 const Paragraph = (props: Props) => {
     return (
         <p id="paragraph">
-            Publishing a book is a great endeavor. There are a lot of a steps to write, produce and market a book. You want to have a quality book at the end…no matter the method chosen to publish and distribute. The <i>Quality Book Formula Checklist</i> was created to help you be aware of the design, overall production process and time lines.
+            Publishing a book is a great endeavor. There are a lot of a steps to write, produce and market a book. You want to have a quality book at the end…no matter the method chosen to publish and distribute. The <i>Quality Book Formula Checklist</i> was created to help you be aware of the design, production process, and overall time lines.
         </p>
     )
 }
