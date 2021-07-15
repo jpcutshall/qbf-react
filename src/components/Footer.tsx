bdbaeca17cceac9854fb914702e05ecf4f19a46a
jpcutshall <jpcutshall@gmail.com>
2021-07-15 19:43:59 -0400

    updated copyright section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ interface Props {
 const Footer = (props: Props) => {
     return (
         <div id="foot">
-            <h5>Copyright © 2021 <a href="https://brigittecutshall.com" target="_blank" rel="noreferrer" >Gemini Media</a> All rights reserved. </h5>
+            <h5> © 2021 <a href="https://brigittecutshall.com" target="_blank" rel="noreferrer" >Brigitte Cutshall</a> All rights reserved. </h5>
         </div>
     )
 }
