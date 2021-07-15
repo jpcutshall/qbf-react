5d6f281d25a1918e9347393e56e5686383c91450
jpcutshall <jpcutshall@gmail.com>
2021-07-15 00:53:20 -0400

    need styling updates to peach colors

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,17 @@ function App() {
 
   const [modalIsOpen, setIsOpen] = useState(false)
   const [emailSubmitted, setIsSubmitted] = useState(false)
+  const [hiddenField, setHiddenField] = useState("")
 
-  const handleHiddenChange = () => {
-    
+  const handleHiddenChange = (event: any) => {
+    console.log(event)
+    setHiddenField(event.target.value)
   }
 
-  const handleEmailSubmission = (e: React.FormEvent) => {
+  const handleEmailSubmission = (e: any) => {
     e.preventDefault()
     setIsSubmitted(true)
-    console.log(e.target)
+    console.log(e.target.email.value)
   }
 
   const handleOpenModal = () => {
@@ -38,20 +40,24 @@ function App() {
         onRequestClose={handleCloseModal}
       >
          
-          <form action="https://brigittecutshall.us11.list-manage.com/subscribe/post?u=7dc72c2e40adfa2d99a20d216&amp;id=44b681c1ed" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate email-modal" target="_blank" >
-            <div id="mc_embed_signup_scroll">
-              <h2>Subscribe</h2>
-              <div className="mc-field-group">
-                <label htmlFor="email">Email Address </label>
-                <input type="email" placeholder="Enter your Email here" name="EMAIL" className="required email" id="email" />
-              </div>
-              <div id="mce-responses" className="clear">
-                <div className="response" id="mce-error-response" ></div>
-                <div className="response" id="mce-success-response" ></div>
-              </div>   {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-                <div aria-hidden="true"><input type="text" name="b_7dc72c2e40adfa2d99a20d216_44b681c1ed" tabIndex={-1} value=""/></div>
-                <div className="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button btn"/></div>
+          <form action="https://brigittecutshall.us11.list-manage.com/subscribe/post?u=7dc72c2e40adfa2d99a20d216&amp;id=44b681c1ed" method="post" id="email-modal" name="subscribe-form" className="validate email-modal" target="_blank" onSubmit={handleEmailSubmission} >
+           
+            <h2><i>Enter your Email to receive your free copy of the Quality Book Formula Checklist</i></h2>
+            
+            <label htmlFor="email">Email Address </label>
+            <input type="email" placeholder="Enter your Email here" name="EMAIL" className="required email" id="email" />
+            
+            <div id="mce-responses" className="clear">
+              <div className="response" id="mce-error-response" ></div>
+              <div className="response" id="mce-success-response" ></div>
+            </div>   {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
+
+            <div id="hidden" aria-hidden="true">
+              <input type="text" name="b_7dc72c2e40adfa2d99a20d216_44b681c1ed" tabIndex={-1} onChange={handleHiddenChange} value={hiddenField} />
             </div>
+
+            <input type="submit" value="Subscribe" name="subscribe" id="submit-btn" className="submit-btn"/>
+           
           </form>
 
       </Modal>
