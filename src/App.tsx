e313a5d4a0550dc87dc60ab6825094fed86b7610
jpcutshall <jpcutshall@gmail.com>
2021-07-15 22:35:36 -0400

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
+import { useState, Suspense, lazy } from 'react';
+import './App.css';
 import Modal from 'react-modal';
 import Title from './components/Title';
-import Graphic from './components/Graphic';
 import Paragraph from './components/Paragraph';
 import Download from './components/Download';
 import WhoAmI from './components/WhoAmI';
 import Horz from './components/Horz';
-import './App.css';
 import Footer from './components/Footer';
+import Loader from './components/Loader';
+const Graphic = lazy(() => import('./components/Graphic'));
 
 Modal.setAppElement('#root')
 
@@ -38,47 +39,51 @@ function App() {
   }
   return (
     <div className="App">
-      <Modal
-        isOpen={modalIsOpen}
-        id="modal"
-        onRequestClose={handleCloseModal}
-      >
-         
-          <form action="https://brigittecutshall.us11.list-manage.com/subscribe/post?u=7dc72c2e40adfa2d99a20d216&amp;id=44b681c1ed" method="post" id="email-modal" name="subscribe-form" className="validate email-modal" target="_blank" onSubmit={handleEmailSubmission} >
-           
-            <h2><i>Enter your Email to receive your free copy of the Quality Book Formula Checklist</i></h2>
+      <Suspense fallback={<Loader/>}>
+
+        <Modal
+          isOpen={modalIsOpen}
+          id="modal"
+          onRequestClose={handleCloseModal}
+        >
+          
+            <form action="https://brigittecutshall.us11.list-manage.com/subscribe/post?u=7dc72c2e40adfa2d99a20d216&amp;id=44b681c1ed" method="post" id="email-modal" name="subscribe-form" className="validate email-modal" target="_blank" onSubmit={handleEmailSubmission} >
             
-            <label htmlFor="email">Email Address </label>
-            <input type="email" placeholder="Enter your Email here" name="EMAIL" className="required email" id="email" />
+              <h2><i>Enter your Email to receive your free copy of the Quality Book Formula Checklist</i></h2>
+              
+              <label htmlFor="email">Email Address </label>
+              <input type="email" placeholder="Enter your Email here" name="EMAIL" className="required email" id="email" />
+              
+              <div id="mce-responses" className="clear">
+                <div className="response" id="mce-error-response" ></div>
+                <div className="response" id="mce-success-response" ></div>
+              </div>   {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
+
+              <div id="hidden" aria-hidden="true">
+                <input type="checkbox" name="group[29862][1]" onChange={(e: any) => setHiddenGroup(e.target.value)} value={hiddenGroup} checked/>
+                <input type="text" name="b_7dc72c2e40adfa2d99a20d216_44b681c1ed" tabIndex={-1} onChange={handleHiddenChange} value={hiddenField} />
+              </div>
+
+              {emailSubmitted ? 
+                <a id="download" href="Qualilty-Book-Formula_Checklist.pdf" download>Download</a> 
+                :
+                null
+              }
+
+              <input type="submit" value="Subscribe" name="subscribe" id="submit-btn" className="submit-btn"/>
             
-            <div id="mce-responses" className="clear">
-              <div className="response" id="mce-error-response" ></div>
-              <div className="response" id="mce-success-response" ></div>
-            </div>   {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
+            </form>
 
-            <div id="hidden" aria-hidden="true">
-              <input type="checkbox" name="group[29862][1]" onChange={(e: any) => setHiddenGroup(e.target.value)} value={hiddenGroup} checked/>
-              <input type="text" name="b_7dc72c2e40adfa2d99a20d216_44b681c1ed" tabIndex={-1} onChange={handleHiddenChange} value={hiddenField} />
-            </div>
+        </Modal>
+        <Title />
+        <Graphic />
+        <Paragraph />
+        <Download handleOpenModal={handleOpenModal}/>
+        <Horz />
+        <WhoAmI />
+        <Footer />
 
-            {emailSubmitted ? 
-              <a id="download" href="Qualilty-Book-Formula_Checklist.pdf" download>Download</a> 
-              :
-              null
-            }
-
-            <input type="submit" value="Subscribe" name="subscribe" id="submit-btn" className="submit-btn"/>
-           
-          </form>
-
-      </Modal>
-      <Title />
-      <Graphic />
-      <Paragraph />
-      <Download handleOpenModal={handleOpenModal}/>
-      <Horz />
-      <WhoAmI />
-      <Footer />
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/Loader.tsx
@@ -0,0 +1,14 @@
+
+interface Props {
+    
+}
+
+const Loader = (props: Props) => {
+    return (
+        <div id="loader">
+            
+        </div>
+    )
+}
+
+export default Loader
