e2b0a07b2c8f042dec8d9d241d82b3d8e8897779
jpcutshall <jpcutshall@gmail.com>
2022-02-08 19:06:21 -0500

    updated paragraph

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -3,10 +3,16 @@ interface Props {}
 const Paragraph = (props: Props) => {
 	return (
 		<div id='paragraph'>
-			<p>
+			{/* <p>
 				&emsp; Publishing a book is a great endeavor. There are a lot of a steps to write, design, produce, and
 				market a book. You want to have a <em>professional quality</em> book at the end…no matter the method
 				chosen to publish and distribute.
+			</p> */}
+			<p>
+				&emsp; The Quality Book Formula Checklist was created to help you be aware of the design, production
+				process, and overall time lines. Follow my checklist to create the book of your dreams. From my personal
+				experience as a writer and publisher. I can't promise your book will be perfect, that is on you! But it
+				will be a quality book created by you!
 			</p>
 		</div>
 	);
