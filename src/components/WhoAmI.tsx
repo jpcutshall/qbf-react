09c166a72e63aa99d3d7d41f3b61a2038e036222
Jacob Cutshall <jpcutshall@gmail.com>
2022-05-07 18:26:54 -0400

    fixed a link to podcast but the brigitcutshall.com scroll routing is broken

--- src/components/WhoAmI.tsx
+++ src/components/WhoAmI.tsx
@@ -1,32 +1,53 @@
-
-interface Props {
-    
-}
+interface Props {}
 
 const WhoAmI = (props: Props) => {
     return (
         <div id="about">
             <figure id="about-img">
-                <img width="150" height="150" src="BCHeadshot.jpg" alt="Brigitte Cutshall wearing a shirt that says The Book Was Better" />
+                <img
+                    width="150"
+                    height="150"
+                    src="BCHeadshot.jpg"
+                    alt="Brigitte Cutshall wearing a shirt that says The Book Was Better"
+                />
                 <figcaption id="img-text">Brigitte Cutshall</figcaption>
             </figure>
             <div id="about-text">
-
-                <h2><i>
-                    <a href="https://www.linkedin.com/in/brigittecutshall/" target="_blank" rel="noreferrer">
-                        Who am I?
-                </a></i></h2>
+                <h2>
+                    <i>
+                        <a
+                            href="https://www.linkedin.com/in/brigittecutshall/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Who am I?
+                        </a>
+                    </i>
+                </h2>
 
                 <p>
-                    Hi there! My name is Brigitte and I’m a Media Solutions & Production Expert, Author, Founder of <a href="http://www.geminimediainc.com/" target="_blank" rel="noreferrer">Gemini Media</a>,
-                    and host of the <a href="https://brigittecutshall.com/podcast/" target="_blank" rel="noreferrer">Real Things Living</a> Podcast.
+                    Hi there! My name is Brigitte and I’m a Media Solutions & Production Expert,
+                    Author, Founder of{' '}
+                    <a href="http://www.geminimediainc.com/" target="_blank" rel="noreferrer">
+                        Gemini Media
+                    </a>
+                    , and host of the{' '}
+                    <a
+                        href="https://brigittecutshall.com/resources/#podcast"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Real Things Living
+                    </a>{' '}
+                    Podcast.
                 </p>
                 <p>
-                    For the past 25+ years, I’ve been helping content creators with production solutions in the business of books.
+                    For the past 25+ years, I’ve been helping content creators with production
+                    solutions in the business of books.
                 </p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default WhoAmI
+export default WhoAmI;
